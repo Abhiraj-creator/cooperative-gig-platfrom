f645a2e8a282df38b022af0db973cae283c40098
Abhiraj-creator <abhirajbhati676@gmail.com>
2026-09-18 20:49:45 +0530

    differnernt logo size according to endpoints

--- frontend/src/shared/components/Logo.tsx
+++ frontend/src/shared/components/Logo.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 
 interface LogoProps {
+  size?: number | string;
   className?: string;
   style?: React.CSSProperties;
 }
 
-export function Logo({ className = '', style }: LogoProps) {
+export function Logo({ size = 79, className = '', style }: LogoProps) {
   return (
     <img
       src="/images/one.png"
       alt="Sahkaar logo"
-      width='79'
-      height='79'
+      width={
+        {
+          '640px': '50px',
+          '768px': '60px',
+          '1024px': '70px',
+          '1280px': '79px',
+          '1536px': '89px',
+          default: '79px',
+        }[size]
+      }
+      height={
+        {
+          '640px': '50px',
+          '768px': '60px',
+          '1024px': '70px',
+          '1280px': '79px',
+          '1536px': '89px',
+          default: '79px',
+        }[size]
+      }
       className={`sahkaar-logo ${className}`}
       style={{
         display: 'inline-block',
